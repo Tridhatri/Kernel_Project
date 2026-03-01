428c5e2d7aa0824a3766355282ca3a141ddd2c08
Tridhatri <tridhatri.sontena@gmail.com>
2026-03-02 02:33:38 +0530

    remove : memset func in kernel.c
    - Moved to common.c

--- kernel.c
+++ kernel.c
@@ -36,13 +36,6 @@ void puts(const char *s) {
     }
 }
 
-void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t *) buf;
-    while (n--) {
-        *p++ = c;
-    }
-    return buf;
-}
 
 void kernel_main(void) {
     // Clear BSS section
